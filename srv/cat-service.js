d41c79c9dfd108420da81c77d8a660c2d2750c2e
Knecht <christoph.knecht@sap.com>
2023-10-09 20:15:13 +0200

    i18n added

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -19,7 +19,7 @@ class CatalogService extends cds.ApplicationService {
 
     grantDiscount(results) {
         for (let b of results) {
-            if (b.stock > 200) { b.title += ' -- 11% discount!'; }
+            if (b.stock > 200) { b.title += ' -- 11% Discount!'; }
         }
 
     }
@@ -29,20 +29,29 @@ class CatalogService extends cds.ApplicationService {
         const { Books } = this.entities;
         const { book, quantity } = req.data;
 
-        if (quantity < 1) { return req.error('The quantity must be at least 1.'); }
+        if (quantity < 1) {
+            // return req.error('The quantity must be at least 1.'); 
+            return req.error('INVALID_QUANTITY');
+        }
 
         const b = await SELECT.one.from(Books).where({ ID: book }).columns(b => { b.stock });
 
-        if (!b) { return req.error(`Book #${book} doesn't exist.`); }
+        if (!b) {
+            // return req.error(`Book with ID ${book} does not exist.`); 
+            return req.error('BOOK_NOT_FOUND', [book]);
+        }
 
         let { stock } = b;
-        if (quantity > stock) { return req.error(`${quantity} exceeds stock ${stock} for book #${book}`); }
+        if (quantity > stock) {
+            // return req.error(`${quantity} exceeds stock ${stock} for book with ID ${book}.`); 
+            return req.error('ORDER_EXCEEDS_STOCK', [quantity, stock, book]);
+        }
 
         await UPDATE(Books).where({ ID: book }).with({ stock: stock -= quantity });
         return { stock };
 
     }
- 
+
 
 }
 
